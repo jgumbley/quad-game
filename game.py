6d761be7994cdd2b5430b985aefae723c160b368
Jim Gumbley <jgumbley@thoughtworks.com>
2013-12-26 12:25:01 +0000

    pull sprite based stuff into class

--- game.py
+++ game.py
@@ -1,10 +1,20 @@
 from pyglet import image, sprite, window, app
 
-quad = image.load('quad.bmp')
-quad_animation = image.ImageGrid(quad, 1, 8)
-quad_sprite = sprite.Sprite(quad_animation[1])
+
+class Quad(sprite.Sprite):
+    """
+    This is a class for the Quad
+    """
+    sprite_sheet = image.load('quad.bmp')
+
+    def __init__(self, window, x, y, scale=1):
+        image_grid = image.ImageGrid(self.sprite_sheet, 1, 8)
+        super(Quad, self).__init__(image_grid[1], x, y, batch=None)
+
+
 
 game_window = window.Window()
+quad_sprite = Quad(game_window, 0,0)
 
 
 @game_window.event
