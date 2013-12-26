54fa2750b95da137e76162efc5d62f5b5e091f44
Jim Gumbley <jgumbley@thoughtworks.com>
2013-12-26 12:31:29 +0000

    move window to a class

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import sys
 from pyglet import image, sprite, window, app
 
 
@@ -7,21 +8,27 @@ class Quad(sprite.Sprite):
     """
     sprite_sheet = image.load('quad.bmp')
 
+
     def __init__(self, window, x, y, scale=1):
         image_grid = image.ImageGrid(self.sprite_sheet, 1, 8)
         super(Quad, self).__init__(image_grid[1], x, y, batch=None)
 
 
+class GameWindow(window.Window):
+    """
+    This is the game window
+    """
 
-game_window = window.Window()
-quad_sprite = Quad(game_window, 0,0)
+    def __init__(self):
+        super(GameWindow, self).__init__()
+        self.quad_sprite = Quad(self, 0, 0)
+        app.run()
 
 
-@game_window.event
-def on_draw():
-    game_window.clear()
-    quad_sprite.draw()
+    def on_draw(self):
+        self.clear()
+        self.quad_sprite.draw()
 
 
 if __name__ == "__main__":
-    app.run()
+    sys.exit(GameWindow())
