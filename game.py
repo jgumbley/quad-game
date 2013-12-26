1c1bb9290cbc4ed13754e92092b1ec80a311b0d3
Jim Gumbley <jgumbley@thoughtworks.com>
2013-12-26 12:46:30 +0000

    no anti-aliasing - like the 80s look

--- game.py
+++ game.py
@@ -1,17 +1,36 @@
 import sys
+import random
 from pyglet import image, sprite, window, app
+from pyglet.gl import (
+    glTexParameteri,
+    glBindTexture,
+    GL_TEXTURE_MAG_FILTER,
+    GL_NEAREST
+)
+
+
+def no_anti_alias(image):
+    texture = image.get_texture()
+    glBindTexture(texture.target, texture.id)
+    glTexParameteri(texture.target, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glBindTexture(texture.target, 0)
+    return texture
 
 
 class Quad(sprite.Sprite):
     """
     This is a class for the Quad
     """
-    sprite_sheet = image.load('quad.bmp')
-
+    sprite_sheet = no_anti_alias(image.load('quad.bmp'))
 
     def __init__(self, window, x, y, scale=1):
         image_grid = image.ImageGrid(self.sprite_sheet, 1, 8)
         super(Quad, self).__init__(image_grid[1], x, y, batch=None)
+        self.scale = scale
+        self.px = x
+        self.py = y
+        self.dx = (random.random() - 0.5) * 1000
+        self.dy = (random.random() - 0.5) * 1000
 
 
 class GameWindow(window.Window):
@@ -21,9 +40,8 @@ class GameWindow(window.Window):
 
     def __init__(self):
         super(GameWindow, self).__init__()
-        self.quad_sprite = Quad(self, 0, 0)
+        self.quad_sprite = Quad(self, 100, 100, scale=3)
         app.run()
-
 
     def on_draw(self):
         self.clear()
